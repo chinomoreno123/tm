7b380a3a3c070e199819c8400d3d4b9de6ca077b
Dariusz Pudlik <dariusz.pudlik@poleng.pl>
2017-08-01 16:05:21 +0200

    Second commit

--- TranslationManagement/src/main/java/pl/poleng/config/HibernateConfiguration.java
+++ TranslationManagement/src/main/java/pl/poleng/config/HibernateConfiguration.java
@@ -8,4 +8,4 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @PropertySource({ "classpath:db.properties" })
 public class HibernateConfiguration {
-}
+} 
